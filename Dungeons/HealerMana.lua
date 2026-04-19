f4c0932b1e153cf97b4def14c0a1c3d4118f3e8e
Nrsken <erikabrah34@gmail.com>
2026-04-19 03:18:41 +0200

    Healer mana module

---
+++ Dungeons/HealerMana.lua
@@ -0,0 +1,365 @@
+-- NorskenUI namespace
+---@class NRSKNUI
+local NRSKNUI = select(2, ...)
+
+-- Check for addon object
+if not NorskenUI then
+    error("HealerMana: Addon object not initialized. Check file load order!")
+    return
+end
+
+-- Create module
+---@class HealerMana: AceModule, AceEvent-3.0, AceTimer-3.0
+local HM = NorskenUI:NewModule("HealerMana", "AceEvent-3.0", "AceTimer-3.0")
+
+-- Localization
+local CreateFrame = CreateFrame
+local UnitExists = UnitExists
+local UnitIsConnected = UnitIsConnected
+local UnitClass = UnitClass
+local UnitName = UnitName
+local UnitPowerPercent = UnitPowerPercent
+local UnitGroupRolesAssigned = UnitGroupRolesAssigned
+local GetSpecializationInfoByID = GetSpecializationInfoByID
+local GetInspectSpecialization = GetInspectSpecialization
+local IsInRaid = IsInRaid
+local IsInGroup = IsInGroup
+local pairs = pairs
+local wipe = wipe
+
+-- Module state
+HM.healerFrames = {}
+HM.containerFrame = nil
+HM.updateTimer = nil
+HM.currentHealer = nil
+
+-- Update db reference
+function HM:UpdateDB()
+    if NRSKNUI.db and NRSKNUI.db.profile then
+        self.db = NRSKNUI.db.profile.HealerMana
+    end
+end
+
+-- Module init
+function HM:OnInitialize()
+    self:UpdateDB()
+    self:SetEnabledState(false)
+end
+
+-- Healing spec icon fallbacks by class
+local HEALER_SPEC_ICONS = {
+    DRUID = 136041,      -- Restoration
+    MONK = 608952,       -- Mistweaver
+    PALADIN = 135920,    -- Holy
+    PRIEST = 135940,     -- Discipline
+    SHAMAN = 136052,     -- Restoration
+    EVOKER = 4622476,    -- Preservation
+}
+
+-- Get spec icon for a spec ID
+local function GetSpecIcon(specID)
+    if not specID or specID == 0 then return nil end
+    local _, _, _, icon = GetSpecializationInfoByID(specID)
+    return icon
+end
+
+-- Check if unit is a healer
+local function IsHealer(unit)
+    local role = UnitGroupRolesAssigned(unit)
+    return role == "HEALER"
+end
+
+-- Display mana percent in the frame
+local function DisplayManaPercent(fontString, unit)
+    local pct = UnitPowerPercent(unit, Enum.PowerType.Mana, true, CurveConstants.ScaleTo100)
+    fontString:SetFormattedText("%.0f%%", pct)
+end
+
+-- Create a healer frame
+function HM:CreateHealerFrame(index)
+    local frame = CreateFrame("Frame", "NorskenUI_HealerMana_" .. index, self.containerFrame)
+    frame:SetSize(self.db.FrameWidth, self.db.IconSize)
+
+    -- Icon container with border
+    frame.iconFrame = CreateFrame("Frame", nil, frame, "BackdropTemplate")
+    frame.iconFrame:SetSize(self.db.IconSize, self.db.IconSize)
+    frame.iconFrame:SetPoint("LEFT", frame, "LEFT", 0, 0)
+    frame.iconFrame:SetBackdrop({
+        bgFile = "Interface\\Buttons\\WHITE8X8",
+        edgeFile = "Interface\\Buttons\\WHITE8X8",
+        edgeSize = 1,
+    })
+    frame.iconFrame:SetBackdropColor(0, 0, 0, 1)
+    frame.iconFrame:SetBackdropBorderColor(0, 0, 0, 1)
+
+    -- Icon texture
+    frame.icon = frame.iconFrame:CreateTexture(nil, "ARTWORK")
+    frame.icon:SetPoint("TOPLEFT", 1, -1)
+    frame.icon:SetPoint("BOTTOMRIGHT", -1, 1)
+
+    -- Name
+    local fontPath = NRSKNUI:GetFontPath(self.db.FontFace)
+    local fontOutline = self.db.FontOutline or "OUTLINE"
+    local useSoftOutline = fontOutline == "SOFTOUTLINE"
+    local actualOutline = useSoftOutline and "" or (fontOutline == "NONE" and "" or fontOutline)
+
+    frame.name = frame:CreateFontString(nil, "OVERLAY")
+    frame.name:SetFont(fontPath, self.db.NameFontSize, actualOutline)
+    frame.name:SetPoint("LEFT", frame.iconFrame, "RIGHT", self.db.NameXOffset, self.db.NameYOffset)
+    frame.name:SetJustifyH("LEFT")
+
+    if useSoftOutline and NRSKNUI.CreateSoftOutline then
+        frame.nameSoftOutline = NRSKNUI:CreateSoftOutline(frame.name, { size = 2 })
+    end
+
+    local manaOutline = (fontOutline == "NONE") and "" or "OUTLINE"
+    frame.mana = frame:CreateFontString(nil, "OVERLAY")
+    frame.mana:SetFont(fontPath, self.db.ManaFontSize, manaOutline)
+    frame.mana:SetPoint("LEFT", frame.iconFrame, "RIGHT", self.db.ManaXOffset, self.db.ManaYOffset)
+    frame.mana:SetJustifyH("LEFT")
+
+    frame:Hide()
+    return frame
+end
+
+-- Get or create healer frame
+function HM:GetHealerFrame(index)
+    if not self.healerFrames[index] then
+        self.healerFrames[index] = self:CreateHealerFrame(index)
+    end
+    return self.healerFrames[index]
+end
+
+-- Update healer frame appearance
+function HM:UpdateFrameAppearance(frame)
+    local fontPath = NRSKNUI:GetFontPath(self.db.FontFace)
+    local fontOutline = self.db.FontOutline
+    local useSoftOutline = fontOutline == "SOFTOUTLINE"
+    local actualOutline = useSoftOutline and "" or (fontOutline == "NONE" and "" or fontOutline)
+    local manaOutline = (fontOutline == "NONE") and "" or "OUTLINE"
+
+    frame:SetSize(self.db.FrameWidth, self.db.IconSize)
+    frame.iconFrame:SetSize(self.db.IconSize, self.db.IconSize)
+    frame.name:SetFont(fontPath, self.db.NameFontSize, actualOutline)
+    frame.name:ClearAllPoints()
+    frame.name:SetPoint("BOTTOMLEFT", frame.iconFrame, "TOPRIGHT", self.db.NameXOffset, self.db.NameYOffset)
+    frame.mana:SetFont(fontPath, self.db.ManaFontSize, manaOutline)
+    frame.mana:ClearAllPoints()
+    frame.mana:SetPoint("TOPLEFT", frame.iconFrame, "BOTTOMRIGHT", self.db.ManaXOffset, self.db.ManaYOffset)
+end
+
+-- Create container frame
+function HM:CreateContainer()
+    if self.containerFrame then return self.containerFrame end
+
+    local frame = CreateFrame("Frame", "NorskenUI_HealerMana_Container", UIParent)
+    frame:SetSize(self.db.FrameWidth, self.db.IconSize)
+    frame:SetFrameStrata(self.db.Strata or "HIGH")
+
+    local pos = self.db.Position
+    frame:ClearAllPoints()
+    frame:SetPoint(pos.AnchorFrom, UIParent, pos.AnchorTo, pos.XOffset, pos.YOffset)
+
+    self.containerFrame = frame
+    return frame
+end
+
+-- Position the healer frame
+function HM:PositionFrame()
+    local frame = self.healerFrames[1]
+    if frame then
+        frame:ClearAllPoints()
+        frame:SetPoint("TOPLEFT", self.containerFrame, "TOPLEFT", 0, 0)
+    end
+    self.containerFrame:SetSize(self.db.FrameWidth, self.db.IconSize)
+end
+
+-- Find and cache the first healer in party
+function HM:FindHealer()
+    if not self.db or not self.db.Enabled then return end
+    local inGroup = IsInGroup()
+    local inRaid = IsInRaid()
+
+    -- Only show in party
+    if not inGroup or inRaid then
+        self.currentHealer = nil
+        if self.healerFrames[1] then
+            self.healerFrames[1]:Hide()
+        end
+        if self.containerFrame then
+            self.containerFrame:Hide()
+        end
+        return
+    end
+
+    -- Find healer
+    local healerUnit, healerName, healerSpecID, healerClass
+
+    if IsHealer("player") then
+        healerUnit = "player"
+    else
+        for i = 1, 4 do
+            local unit = "party" .. i
+            if UnitExists(unit) and UnitIsConnected(unit) and IsHealer(unit) then
+                healerUnit = unit
+                break
+            end
+        end
+    end
+
+    -- No healer found
+    if not healerUnit then
+        self.currentHealer = nil
+        if self.healerFrames[1] then
+            self.healerFrames[1]:Hide()
+        end
+        if self.containerFrame then
+            self.containerFrame:Hide()
+        end
+        return
+    end
+
+    -- Cache healer info
+    healerName = UnitName(healerUnit)
+    healerSpecID = GetInspectSpecialization(healerUnit)
+    _, healerClass = UnitClass(healerUnit)
+
+    self.currentHealer = {
+        unit = healerUnit,
+        name = healerName,
+        specID = healerSpecID,
+        class = healerClass,
+        classColor = NRSKNUI:GetClassColor(healerClass),
+    }
+
+    -- Update frame with static info
+    self:UpdateHealerFrame()
+end
+
+-- Update healer frame with cached info
+function HM:UpdateHealerFrame()
+    local healer = self.currentHealer
+    if not healer then return end
+    local frame = self:GetHealerFrame(1)
+
+    -- Set icon
+    local icon = GetSpecIcon(healer.specID)
+    if icon then
+        frame.icon:SetTexture(icon)
+        frame.icon:SetTexCoord(0, 1, 0, 1)
+        if NRSKNUI.ApplyZoom then
+            NRSKNUI:ApplyZoom(frame.icon, 0.3)
+        end
+    else
+        -- Fallback to healing spec icon if spec not available yet
+        local fallbackIcon = HEALER_SPEC_ICONS[healer.class]
+        if fallbackIcon then
+            frame.icon:SetTexture(fallbackIcon)
+            frame.icon:SetTexCoord(0, 1, 0, 1)
+            if NRSKNUI.ApplyZoom then
+                NRSKNUI:ApplyZoom(frame.icon, 0.3)
+            end
+        end
+    end
+
+    -- Set name with class color
+    frame.name:SetText(healer.name)
+    frame.name:SetTextColor(healer.classColor[1], healer.classColor[2], healer.classColor[3])
+
+    -- Set mana color
+    local manaColor = self.db.HighManaColor
+    frame.mana:SetTextColor(manaColor[1], manaColor[2], manaColor[3])
+
+    -- Update mana value
+    DisplayManaPercent(frame.mana, healer.unit)
+
+    -- Position and show
+    self:PositionFrame()
+    frame:Show()
+    self.containerFrame:Show()
+end
+
+-- Update only mana text
+function HM:UpdateMana()
+    local healer = self.currentHealer
+    if not healer then return end
+
+    local frame = self.healerFrames[1]
+    if not frame or not frame:IsShown() then return end
+
+    DisplayManaPercent(frame.mana, healer.unit)
+end
+
+-- Apply settings
+function HM:ApplySettings()
+    self:UpdateDB()
+    if not self.db or not self.db.Enabled then
+        if self.containerFrame then
+            self.containerFrame:Hide()
+        end
+        return
+    end
+
+    -- Create/update container
+    self:CreateContainer()
+
+    -- Update container position
+    local pos = self.db.Position
+    self.containerFrame:ClearAllPoints()
+    self.containerFrame:SetPoint(pos.AnchorFrom, UIParent, pos.AnchorTo, pos.XOffset, pos.YOffset)
+    self.containerFrame:SetFrameStrata(self.db.Strata or "HIGH")
+
+    -- Force update
+    self:FindHealer()
+end
+
+-- Refresh all frames
+function HM:Refresh()
+    -- Clear cache
+    self.currentHealer = nil
+
+    -- Hide and clear all frames
+    for _, frame in pairs(self.healerFrames) do frame:Hide() end
+    wipe(self.healerFrames)
+
+    -- Recreate container
+    if self.containerFrame then
+        self.containerFrame:Hide()
+        self.containerFrame = nil
+    end
+
+    self:ApplySettings()
+end
+
+-- Start update timer for mana checks on the healer
+function HM:StartUpdates()
+    if self.updateTimer then return end
+    self.updateTimer = self:ScheduleRepeatingTimer("UpdateMana", 1)
+end
+
+-- Stop update timer
+function HM:StopUpdates()
+    if self.updateTimer then
+        self:CancelTimer(self.updateTimer)
+        self.updateTimer = nil
+    end
+end
+
+-- Module OnEnable
+function HM:OnEnable()
+    self:UpdateDB()
+    if not self.db or not self.db.Enabled then return end
+    self:ApplySettings()
+    self:StartUpdates()
+    self:RegisterEvent("GROUP_ROSTER_UPDATE", "FindHealer")
+    self:RegisterEvent("PLAYER_ENTERING_WORLD", "FindHealer")
+end
+
+-- Module OnDisable
+function HM:OnDisable()
+    self:StopUpdates()
+    self:UnregisterAllEvents()
+    self.currentHealer = nil
+    if self.containerFrame then self.containerFrame:Hide() end
+    for _, frame in pairs(self.healerFrames) do frame:Hide() end
+end
---
+++ GUI/GUITabs/GUI-Dungeons/GUI-HealerMana.lua
@@ -0,0 +1,285 @@
+-- NorskenUI namespace
+---@class NRSKNUI
+local NRSKNUI = select(2, ...)
+local GUIFrame = NRSKNUI.GUIFrame
+local Theme = NRSKNUI.Theme
+local LSM = NRSKNUI.LSM or LibStub("LibSharedMedia-3.0", true)
+
+-- Localization
+local table_insert = table.insert
+local ipairs = ipairs
+
+-- Helper to get HealerMana module
+local function GetModule()
+    if NorskenUI then
+        return NorskenUI:GetModule("HealerMana", true)
+    end
+    return nil
+end
+
+-- Register HealerMana tab content
+GUIFrame:RegisterContent("HealerMana", function(scrollChild, yOffset)
+    -- Safety check for database
+    local db = NRSKNUI.db and NRSKNUI.db.profile.HealerMana
+    if not db then
+        local errorCard = GUIFrame:CreateCard(scrollChild, "Error", yOffset)
+        errorCard:AddLabel("Database not available")
+        return yOffset + errorCard:GetContentHeight() + Theme.paddingMedium
+    end
+
+    local HM = GetModule()
+
+    -- Apply settings
+    local function ApplySettings()
+        if HM then
+            HM:UpdateDB()
+            HM:ApplySettings()
+        end
+    end
+
+    -- Refresh (recreate frames)
+    local function Refresh()
+        if HM then
+            HM:Refresh()
+        end
+    end
+
+    -- Helper to apply enable state
+    local function ApplyEnableState(enabled)
+        if not HM then return end
+        HM.db.Enabled = enabled
+        if enabled then
+            NorskenUI:EnableModule("HealerMana")
+        else
+            NorskenUI:DisableModule("HealerMana")
+        end
+    end
+
+    -- Track widgets for enable/disable logic
+    local allWidgets = {}
+
+    -- Update all widget states based on main toggle
+    local function UpdateAllWidgetStates()
+        local mainEnabled = db.Enabled ~= false
+        for _, widget in ipairs(allWidgets) do
+            if widget.SetEnabled then
+                widget:SetEnabled(mainEnabled)
+            end
+        end
+    end
+
+    ----------------------------------------------------------------
+    -- Card 1: General Settings
+    ----------------------------------------------------------------
+    local card1 = GUIFrame:CreateCard(scrollChild, "Healer Mana Tracker", yOffset)
+
+    -- Enable Checkbox
+    local row1 = GUIFrame:CreateRow(card1.content, 36)
+    local enableCheck = GUIFrame:CreateCheckbox(row1, "Enable Healer Mana Tracker", db.Enabled ~= false,
+        function(checked)
+            db.Enabled = checked
+            ApplyEnableState(checked)
+            UpdateAllWidgetStates()
+        end,
+        true,
+        "Healer Mana",
+        "On",
+        "Off"
+    )
+    row1:AddWidget(enableCheck, 0.5)
+    card1:AddRow(row1, 36)
+
+    yOffset = yOffset + card1:GetContentHeight() + Theme.paddingSmall
+
+    ----------------------------------------------------------------
+    -- Card 2: Size Settings
+    ----------------------------------------------------------------
+    local card2 = GUIFrame:CreateCard(scrollChild, "Size Settings", yOffset)
+    table_insert(allWidgets, card2)
+
+    -- Icon Size
+    local rowIcon = GUIFrame:CreateRow(card2.content, 36)
+    local iconSlider = GUIFrame:CreateSlider(rowIcon, "Icon Size", 16, 64, 1, db.IconSize, 30, function(value)
+        db.IconSize = value
+        Refresh()
+    end)
+    rowIcon:AddWidget(iconSlider, 1)
+    table_insert(allWidgets, iconSlider)
+    card2:AddRow(rowIcon, 36)
+
+    yOffset = yOffset + card2:GetContentHeight() + Theme.paddingSmall
+
+    ----------------------------------------------------------------
+    -- Card 3: Name Text Settings
+    ----------------------------------------------------------------
+    local card3 = GUIFrame:CreateCard(scrollChild, "Name Text", yOffset)
+    table_insert(allWidgets, card3)
+
+    -- Name Font Size + X Offset
+    local rowName1 = GUIFrame:CreateRow(card3.content, 36)
+    local nameSizeSlider = GUIFrame:CreateSlider(rowName1, "Font Size", 8, 44, 1, db.NameFontSize, 30, function(value)
+        db.NameFontSize = value
+        Refresh()
+    end)
+    rowName1:AddWidget(nameSizeSlider, 1)
+    table_insert(allWidgets, nameSizeSlider)
+    card3:AddRow(rowName1, 36)
+
+    -- Separator
+    local sepRow = GUIFrame:CreateRow(card3.content, 8)
+    local sep = GUIFrame:CreateSeparator(sepRow)
+    sepRow:AddWidget(sep, 1)
+    card3:AddRow(sepRow, 8)
+
+    local rowName3 = GUIFrame:CreateRow(card3.content, 36)
+    local nameXSlider = GUIFrame:CreateSlider(rowName3, "X Offset", -40, 40, 1, db.NameXOffset, 30, function(value)
+        db.NameXOffset = value
+        Refresh()
+    end)
+    rowName3:AddWidget(nameXSlider, 0.5)
+    table_insert(allWidgets, nameXSlider)
+
+    -- Name Y Offset
+    local nameYSlider = GUIFrame:CreateSlider(rowName3, "Y Offset", -40, 40, 1, db.NameYOffset, 30, function(value)
+        db.NameYOffset = value
+        Refresh()
+    end)
+    rowName3:AddWidget(nameYSlider, 0.5)
+    table_insert(allWidgets, nameYSlider)
+    card3:AddRow(rowName3, 36)
+
+    yOffset = yOffset + card3:GetContentHeight() + Theme.paddingSmall
+
+    ----------------------------------------------------------------
+    -- Card 4: Mana Text Settings
+    ----------------------------------------------------------------
+    local card4 = GUIFrame:CreateCard(scrollChild, "Mana Text", yOffset)
+    table_insert(allWidgets, card4)
+
+    -- Mana Font Size + X Offset
+    local rowMana1 = GUIFrame:CreateRow(card4.content, 36)
+    local manaSizeSlider = GUIFrame:CreateSlider(rowMana1, "Font Size", 8, 44, 1, db.ManaFontSize, 30, function(value)
+        db.ManaFontSize = value
+        Refresh()
+    end)
+    rowMana1:AddWidget(manaSizeSlider, 1)
+    table_insert(allWidgets, manaSizeSlider)
+    card4:AddRow(rowMana1, 36)
+
+    -- Separator
+    local sepRow2 = GUIFrame:CreateRow(card4.content, 8)
+    local sep2 = GUIFrame:CreateSeparator(sepRow2)
+    sepRow2:AddWidget(sep2, 1)
+    card4:AddRow(sepRow2, 8)
+
+    local rowMana2 = GUIFrame:CreateRow(card4.content, 36)
+    local manaXSlider = GUIFrame:CreateSlider(rowMana2, "X Offset", -40, 40, 1, db.ManaXOffset, 30, function(value)
+        db.ManaXOffset = value
+        Refresh()
+    end)
+    rowMana2:AddWidget(manaXSlider, 0.5)
+    table_insert(allWidgets, manaXSlider)
+
+    -- Mana Y Offset
+    local manaYSlider = GUIFrame:CreateSlider(rowMana2, "Y Offset", -40, 40, 1, db.ManaYOffset, 30, function(value)
+        db.ManaYOffset = value
+        Refresh()
+    end)
+    rowMana2:AddWidget(manaYSlider, 0.5)
+    table_insert(allWidgets, manaYSlider)
+    card4:AddRow(rowMana2, 36)
+
+    yOffset = yOffset + card4:GetContentHeight() + Theme.paddingSmall
+
+    ----------------------------------------------------------------
+    -- Card 5: Mana Text Color
+    ----------------------------------------------------------------
+    local card5 = GUIFrame:CreateCard(scrollChild, "Mana Text Color", yOffset)
+    table_insert(allWidgets, card5)
+
+    -- Mana Color
+    local rowColor = GUIFrame:CreateRow(card5.content, 37)
+    local manaColorPicker = GUIFrame:CreateColorPicker(rowColor, "Mana Text Color", db.HighManaColor,
+        function(r, g, b, a)
+            db.HighManaColor = { r, g, b, a }
+            ApplySettings()
+        end)
+    rowColor:AddWidget(manaColorPicker, 0.5)
+    table_insert(allWidgets, manaColorPicker)
+    card5:AddRow(rowColor, 37)
+
+    yOffset = yOffset + card5:GetContentHeight() + Theme.paddingSmall
+
+    ----------------------------------------------------------------
+    -- Card 6: Font Settings
+    ----------------------------------------------------------------
+    local card6 = GUIFrame:CreateCard(scrollChild, "Font Settings", yOffset)
+    table_insert(allWidgets, card6)
+
+    -- Font Face + Outline
+    local fontList = {}
+    if LSM then
+        for name in pairs(LSM:HashTable("font")) do fontList[name] = name end
+    else
+        fontList["Friz Quadrata TT"] = "Friz Quadrata TT"
+    end
+
+    local rowFont = GUIFrame:CreateRow(card6.content, 40)
+    local fontDropdown = GUIFrame:CreateDropdown(rowFont, "Font", fontList, db.FontFace or "Expressway", 30,
+        function(value)
+            db.FontFace = value
+            Refresh()
+        end, true)
+    rowFont:AddWidget(fontDropdown, 0.5)
+    table_insert(allWidgets, fontDropdown)
+
+    local outlineList = {
+        { key = "NONE",         text = "None" },
+        { key = "OUTLINE",      text = "Outline" },
+        { key = "THICKOUTLINE", text = "Thick" },
+        { key = "SOFTOUTLINE",  text = "Soft" },
+    }
+    local outlineDropdown = GUIFrame:CreateDropdown(rowFont, "Outline", outlineList, db.FontOutline or "SOFTOUTLINE", 30,
+        function(value)
+            db.FontOutline = value
+            Refresh()
+        end)
+    rowFont:AddWidget(outlineDropdown, 0.5)
+    table_insert(allWidgets, outlineDropdown)
+    card6:AddRow(rowFont, 40)
+
+    yOffset = yOffset + card6:GetContentHeight() + Theme.paddingSmall
+
+    ----------------------------------------------------------------
+    -- Card 7: Position
+    ----------------------------------------------------------------
+    local card7, newOffset = GUIFrame:CreatePositionCard(scrollChild, yOffset, {
+        db = db,
+        dbKeys = {
+            anchorFrameType = "anchorFrameType",
+            anchorFrameFrame = "ParentFrame",
+            selfPoint = "AnchorFrom",
+            anchorPoint = "AnchorTo",
+            xOffset = "XOffset",
+            yOffset = "YOffset",
+            strata = "Strata",
+        },
+        showAnchorFrameType = true,
+        showStrata = true,
+        onChangeCallback = ApplySettings,
+    })
+
+    if card7.positionWidgets then
+        for _, widget in ipairs(card7.positionWidgets) do
+            table_insert(allWidgets, widget)
+        end
+    end
+    table_insert(allWidgets, card7)
+
+    yOffset = newOffset
+
+    -- Apply initial widget states
+    UpdateAllWidgetStates()
+    yOffset = yOffset - Theme.paddingSmall
+    return yOffset
+end)
