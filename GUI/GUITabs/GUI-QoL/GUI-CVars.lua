9cf4c3d3ef420380cf68410563fee61b0a9b9515
Nrsken <erikabrah34@gmail.com>
2026-05-29 17:19:57 +0200

    GUI Improvements

--- GUI/GUITabs/GUI-QoL/GUI-CVars.lua
+++ GUI/GUITabs/GUI-QoL/GUI-CVars.lua
@@ -13,65 +13,119 @@ GUIFrame:RegisterContent("MiscVars", function(scrollChild, yOffset)
     local manager = GUIFrame:CreateWidgetStateManager()
     local function UpdateAllWidgetStates() manager:UpdateAll(db.Enabled ~= false) end
 
-    local card1 = GUIFrame:CreateCard(scrollChild, "CVar Browser", yOffset)
+    local function CreateCVarWidget(card, def)
+        local key = def.key
+        local widgetRow = GUIFrame:CreateRow(card.content, Theme.rowHeight)
+        local widget
+        local tooltipConfig = def.description and { text = def.description, default = def.default, } or nil
 
-    if MVAR then
-        for i, def in ipairs(MVAR.DEFS) do
-            local key = def.key
-
-            local widgetRow = GUIFrame:CreateRow(card1.content, Theme.rowHeight)
-            local widget
-
-            local tooltipConfig = def.description and { text = def.description, default = def.default, } or nil
-
-            -- For cvars that simply use 0/1 to disable/enable we use checkboxes
-            if def.type == "boolean" then
-                widget = GUIFrame:CreateCheckbox(widgetRow, def.label, {
-                    value = db[key],
-                    tooltip = tooltipConfig,
-                    cvartooltip = true,
-                    callback = function(checked)
-                        db[key] = checked
-                        MVAR._suppressCVarUpdate = true
-                        MVAR:ApplySettings()
-                        MVAR._suppressCVarUpdate = false
-                    end
-                })
-                widgetRow:AddWidget(widget, 1)
-
-                -- For cvars that have a range of values we use sliders
-            elseif def.type == "number" then
-                widget = GUIFrame:CreateSlider(widgetRow, def.label, {
-                    min = def.min,
-                    max = def.max,
-                    step = def.step,
-                    value = db[key],
-                    tooltip = tooltipConfig,
-                    cvartooltip = true,
-                    callback = function(value)
-                        db[key] = value
-                        MVAR._suppressCVarUpdate = true
-                        MVAR:ApplySettings()
-                        MVAR._suppressCVarUpdate = false
-                    end
-                })
-                widgetRow:AddWidget(widget, 1)
-            end
-
-            manager:Register(widget, "all")
-            card1:AddRow(widgetRow, Theme.rowHeight)
-
-            if i < #MVAR.DEFS then
-                local sepRow = GUIFrame:CreateRow(card1.content, Theme.rowHeightSeparator)
-                local sep = GUIFrame:CreateSeparator(sepRow)
-                sepRow:AddWidget(sep, 1)
-                manager:Register(sep, "all")
-                card1:AddRow(sepRow, Theme.rowHeightSeparator)
-            end
+        if def.type == "boolean" then
+            widget = GUIFrame:CreateCheckbox(widgetRow, def.label, {
+                value = db[key],
+                tooltip = tooltipConfig,
+                cvartooltip = true,
+                callback = function(checked)
+                    db[key] = checked
+                    MVAR._suppressCVarUpdate = true
+                    MVAR:ApplySettings()
+                    MVAR._suppressCVarUpdate = false
+                end
+            })
+            widgetRow:AddWidget(widget, 1)
+        elseif def.type == "number" then
+            widget = GUIFrame:CreateSlider(widgetRow, def.label, {
+                min = def.min,
+                max = def.max,
+                step = def.step,
+                value = db[key],
+                tooltip = tooltipConfig,
+                cvartooltip = true,
+                callback = function(value)
+                    db[key] = value
+                    MVAR._suppressCVarUpdate = true
+                    MVAR:ApplySettings()
+                    MVAR._suppressCVarUpdate = false
+                end
+            })
+            widgetRow:AddWidget(widget, 1)
         end
+
+        manager:Register(widget, "all")
+        card:AddRow(widgetRow, Theme.rowHeight)
+        return widget
     end
 
-    yOffset = card1:GetNextOffset()
+    local function AddSeparator(card)
+        local sepRow = GUIFrame:CreateRow(card.content, Theme.rowHeightSeparator)
+        local sep = GUIFrame:CreateSeparator(sepRow)
+        sepRow:AddWidget(sep, 1)
+        manager:Register(sep, "all")
+        card:AddRow(sepRow, Theme.rowHeightSeparator)
+    end
+
+    if MVAR then
+        local generalDefs, spellqueueDefs, devDefs = {}, {}, {}
+        for _, def in ipairs(MVAR.DEFS) do
+            if def.category == "spellqueue" then
+                spellqueueDefs[#spellqueueDefs + 1] = def
+            elseif def.category == "dev" then
+                devDefs[#devDefs + 1] = def
+            else
+                generalDefs[#generalDefs + 1] = def
+            end
+        end
+
+        local card1 = GUIFrame:CreateCard(scrollChild, "CVar Browser", yOffset)
+        for i, def in ipairs(generalDefs) do
+            CreateCVarWidget(card1, def)
+            if i < #generalDefs then AddSeparator(card1) end
+        end
+        yOffset = card1:GetNextOffset()
+
+        local card2 = GUIFrame:CreateCard(scrollChild, "Spell Queue Window", yOffset)
+        for i, def in ipairs(spellqueueDefs) do
+            CreateCVarWidget(card2, def)
+            if i < #spellqueueDefs then AddSeparator(card2) end
+        end
+
+        AddSeparator(card2)
+
+        local infoHeight = 70
+        local infoRow = GUIFrame:CreateRow(card2.content, infoHeight)
+        local infoText = GUIFrame:CreateText(infoRow, NRSKNUI:ColorTextByTheme("Learn More"), {
+            text =
+            "The spell queue window determines how early you can\nqueue your next ability before your current cast finishes.\nVisit |cff8788EEXerwo|r's maxroll guide for more information.",
+            height = infoHeight,
+            bgMode = "hide"
+        })
+        infoRow:AddWidget(infoText, 0.65)
+        manager:Register(infoText, "all")
+
+        local linkBtn = GUIFrame:CreateButton(infoRow, "Open Guide", {
+            callback = function()
+                NRSKNUI:CreateCopyDialog(
+                    "Spell Queue Window Guide",
+                    "https://maxroll.gg/wow/resources/spell-queue-window",
+                    "Copy to clipboard by pressing CTRL + C"
+                )
+            end,
+            width = 100,
+            height = 36,
+        })
+        infoRow:AddWidget(linkBtn, 0.35)
+        manager:Register(linkBtn, "all")
+        card2:AddRow(infoRow, infoHeight, 0)
+
+        yOffset = card2:GetNextOffset()
+
+        local card3 = GUIFrame:CreateCard(scrollChild, "Dev CVars", yOffset)
+        for i, def in ipairs(devDefs) do
+            CreateCVarWidget(card3, def)
+            if i < #devDefs then AddSeparator(card3) end
+        end
+        yOffset = card3:GetNextOffset()
+    end
+
     UpdateAllWidgetStates()
     return yOffset
 end)
--- GUI/GUITabs/GUI-QoL/GUI-CharacterPanel.lua
+++ GUI/GUITabs/GUI-QoL/GUI-CharacterPanel.lua
@@ -14,7 +14,7 @@ GUIFrame:RegisterContent("CharacterPanel", function(scrollChild, yOffset)
     manager:SetCondition("gemUtil", function() return db.GemSocketHelper.Enabled end)
     local function UpdateAllWidgetStates() manager:UpdateAll(db.Enabled) end
 
-    -- Card 1
+    -- Card 1: Enable + Recommended
     local card1 = GUIFrame:CreateCard(scrollChild, "Character Panel", yOffset)
 
     local row1 = GUIFrame:CreateRow(card1.content, Theme.rowHeight)
@@ -36,7 +36,43 @@ GUIFrame:RegisterContent("CharacterPanel", function(scrollChild, yOffset)
     local sep1 = GUIFrame:CreateSeparator(card1.content)
     card1:AddRow(sep1, Theme.rowHeightSeparator)
 
-    local row2 = GUIFrame:CreateRow(card1.content, Theme.rowHeight)
+    local recHeight = 130
+    local recRow = GUIFrame:CreateRow(card1.content, recHeight)
+    local recText = GUIFrame:CreateText(recRow, NRSKNUI:ColorTextByTheme("Recommended"), {
+        text = ("BetterCharacterPanel by |cffC41E3AGrim|r" ..
+            "\nAdds additional info to the character panel.\n\n" ..
+            NRSKNUI:ColorTextByTheme("Features") .. "\n" ..
+            NRSKNUI:ColorTextByTheme("• ") .. "Shows ilvl information\n" ..
+            NRSKNUI:ColorTextByTheme("• ") .. "Shows enchant information\n" ..
+            NRSKNUI:ColorTextByTheme("• ") .. "Shows gem information\n"),
+        height = recHeight,
+        bgMode = "hide"
+    })
+    recRow:AddWidget(recText, 0.6)
+    manager:Register(recText, "all")
+
+    local recBtn = GUIFrame:CreateButton(recRow, "Get Addon Here", {
+        callback = function()
+            NRSKNUI:CreateCopyDialog(
+                "BetterCharacterPanel by |cffC41E3AGrim|r",
+                "https://www.curseforge.com/wow/addons/bettercharacterpanel",
+                "Copy to clipboard by pressing CTRL + C"
+            )
+        end,
+        width = 100,
+        height = 36,
+    })
+    recRow:AddWidget(recBtn, 0.4)
+    manager:Register(recBtn, "all")
+    card1:AddRow(recRow, recHeight, 0)
+
+    yOffset = card1:GetNextOffset()
+
+    -- Card 2: Display Options
+    local card2 = GUIFrame:CreateCard(scrollChild, "Display Options", yOffset)
+    manager:Register(card2, "all")
+
+    local row2 = GUIFrame:CreateRow(card2.content, Theme.rowHeight)
     local decimalCheck = GUIFrame:CreateCheckbox(row2, "Show Decimal Item Level", {
         value = db.DecimalItemLevel,
         callback = function(checked)
@@ -49,9 +85,13 @@ GUIFrame:RegisterContent("CharacterPanel", function(scrollChild, yOffset)
     })
     row2:AddWidget(decimalCheck, 1)
     manager:Register(decimalCheck, "all")
-    card1:AddRow(row2, Theme.rowHeight)
+    card2:AddRow(row2, Theme.rowHeight)
 
-    local row2b = GUIFrame:CreateRow(card1.content, Theme.rowHeightLast)
+    local sep2 = GUIFrame:CreateSeparator(card2.content)
+    card2:AddRow(sep2, Theme.rowHeightSeparator)
+    manager:Register(sep2, "all")
+
+    local row2b = GUIFrame:CreateRow(card2.content, Theme.rowHeight)
     local raceTextCheck = GUIFrame:CreateCheckbox(row2b, "Show Race Text", {
         value = db.ShowRaceText,
         callback = function(checked)
@@ -68,16 +108,41 @@ GUIFrame:RegisterContent("CharacterPanel", function(scrollChild, yOffset)
     })
     row2b:AddWidget(raceTextCheck, 1)
     manager:Register(raceTextCheck, "all")
-    card1:AddRow(row2b, Theme.rowHeightLast, 0)
+    card2:AddRow(row2b, Theme.rowHeight)
 
-    yOffset = card1:GetNextOffset()
+    local sep2b = GUIFrame:CreateSeparator(card2.content)
+    card2:AddRow(sep2b, Theme.rowHeightSeparator)
+    manager:Register(sep2b, "all")
 
-    -- Card 2: Gem Socket Helper
+    local trackDb = db.TrackIndicators
+    local row2c = GUIFrame:CreateRow(card2.content, Theme.rowHeightLast)
+    local trackEnableCheck = GUIFrame:CreateCheckbox(row2c, "Show Item Track Letters", {
+        value = trackDb.Enabled,
+        callback = function(checked)
+            trackDb.Enabled = checked
+            if CharacterPanel then
+                if checked then
+                    CharacterPanel:SetupTrackIndicators()
+                    CharacterPanel:UpdateAllTrackIndicators()
+                else
+                    CharacterPanel:HideAllTrackIndicators()
+                end
+            end
+        end,
+        tooltip = "Shows M/H/C/V/A letters on gear slots indicating Myth/Hero/Champion/Veteran/Adventurer tracks",
+    })
+    row2c:AddWidget(trackEnableCheck, 1)
+    manager:Register(trackEnableCheck, "all")
+    card2:AddRow(row2c, Theme.rowHeightLast, 0)
+
+    yOffset = card2:GetNextOffset()
+
+    -- Card 3: Gem Socket Helper
     local gemDb = db.GemSocketHelper
-    local card2 = GUIFrame:CreateCard(scrollChild, "Gem Socket Helper", yOffset)
-    manager:Register(card2, "all")
+    local card3 = GUIFrame:CreateCard(scrollChild, "Gem Socket Helper", yOffset)
+    manager:Register(card3, "all")
 
-    local row3 = GUIFrame:CreateRow(card2.content, Theme.rowHeight)
+    local row3 = GUIFrame:CreateRow(card3.content, Theme.rowHeight)
     local gemEnableCheck = GUIFrame:CreateCheckbox(row3, "Enable Gem Socket Helper", {
         value = gemDb.Enabled,
         callback = function(checked)
@@ -95,12 +160,12 @@ GUIFrame:RegisterContent("CharacterPanel", function(scrollChild, yOffset)
         tooltip = "Shows equipped gem sockets on the character panel with quick gem replacement",
     })
     row3:AddWidget(gemEnableCheck, 1)
-    card2:AddRow(row3, Theme.rowHeight)
+    card3:AddRow(row3, Theme.rowHeight)
 
-    local sep2 = GUIFrame:CreateSeparator(card2.content)
-    card2:AddRow(sep2, Theme.rowHeightSeparator)
+    local sep3 = GUIFrame:CreateSeparator(card3.content)
+    card3:AddRow(sep3, Theme.rowHeightSeparator)
 
-    local row4 = GUIFrame:CreateRow(card2.content, Theme.rowHeight)
+    local row4 = GUIFrame:CreateRow(card3.content, Theme.rowHeight)
     local sizeSlider = GUIFrame:CreateSlider(row4, "Socket Button Size", {
         min = 16,
         max = 48,
@@ -130,9 +195,9 @@ GUIFrame:RegisterContent("CharacterPanel", function(scrollChild, yOffset)
     })
     row4:AddWidget(spacingSlider, 0.5)
     manager:Register(spacingSlider, "all", "gemUtil")
-    card2:AddRow(row4, Theme.rowHeight)
+    card3:AddRow(row4, Theme.rowHeight)
 
-    local row5 = GUIFrame:CreateRow(card2.content, Theme.rowHeightLast)
+    local row5 = GUIFrame:CreateRow(card3.content, Theme.rowHeightLast)
     local emptyOnlyCheck = GUIFrame:CreateCheckbox(row5, "Show Only Empty Sockets", {
         value = gemDb.ShowOnlyEmpty,
         callback = function(checked)
@@ -145,34 +210,7 @@ GUIFrame:RegisterContent("CharacterPanel", function(scrollChild, yOffset)
     })
     row5:AddWidget(emptyOnlyCheck, 1)
     manager:Register(emptyOnlyCheck, "all", "gemUtil")
-    card2:AddRow(row5, Theme.rowHeightLast, 0)
-
-    yOffset = card2:GetNextOffset()
-
-    -- Card 3: Track Indicators
-    local trackDb = db.TrackIndicators
-    local card3 = GUIFrame:CreateCard(scrollChild, "Item Track Indicators", yOffset)
-    manager:Register(card3, "all")
-
-    local row6 = GUIFrame:CreateRow(card3.content, Theme.rowHeightLast)
-    local trackEnableCheck = GUIFrame:CreateCheckbox(row6, "Show Item Track Letters", {
-        value = trackDb.Enabled,
-        callback = function(checked)
-            trackDb.Enabled = checked
-            if CharacterPanel then
-                if checked then
-                    CharacterPanel:SetupTrackIndicators()
-                    CharacterPanel:UpdateAllTrackIndicators()
-                else
-                    CharacterPanel:HideAllTrackIndicators()
-                end
-            end
-        end,
-        tooltip = "Shows M/H/C/V/A letters on gear slots indicating Myth/Hero/Champion/Veteran/Adventurer tracks",
-    })
-    row6:AddWidget(trackEnableCheck, 1)
-    manager:Register(trackEnableCheck, "all")
-    card3:AddRow(row6, Theme.rowHeightLast, 0)
+    card3:AddRow(row5, Theme.rowHeightLast, 0)
 
     yOffset = card3:GetNextOffset()
 
